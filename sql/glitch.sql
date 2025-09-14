dc97fc3a241aa05dc830f5239837379994cd7f33
nwoik <nwokoyed@gmail.com>
2025-09-14 16:21:06 +0100

    sql for tables made

---
+++ sql/glitch.sql
@@ -0,0 +1,32 @@
+CREATE TABLE
+    IF NOT EXISTS glitch (track TEXT);
+
+INSERT INTO
+    glitch (track)
+VALUES
+    ('Wii Mushroom Gorge'),
+    ('Toad''s Temple'),
+    ('Wii Koopa Cape'),
+    ('GCN Waluigi Stadium'),
+    ('Wii Grumble Volcano'),
+    ('Tau-Cryovolcano'),
+    ('N64 Sherbet Land'),
+    ('Frantic Funyard'),
+    ('GCN Daisy Cruiser'),
+    ('Wii Wario''s Gold Mine'),
+    ('The Grand Archives'),
+    ('Shy Guy Lumber Co.'),
+    ('Wii Coconut Mall'),
+    ('Ice Cream Fortress'),
+    ('Bowser Jr.''s Crafty Castle'),
+    ('N64 Bowser''s Castle'),
+    ('DS Airship Fortress'),
+    ('Ruinated Peach''s Castle'),
+    ('N64 DK''s Jungle Parkway'),
+    ('Wii Maple Treeway'),
+    ('Volcanic Skyway'),
+    ('GBA Bowser Castle 4'),
+    ('Obstagoon''s Palace'),
+    ('Spooky Swamp'),
+    ('Wii Rainbow Road'),
+    ('Cosmic Causeway');
---
+++ sql/shortcut.sql
@@ -0,0 +1,23 @@
+CREATE TABLE
+    IF NOT EXISTS shortcut (track TEXT);
+
+INSERT INTO
+    shortcut (track)
+VALUES
+    ('Wii Mushroom Gorge'),
+    ('Toad''s Temple'),
+    ('Wii Grumble Volcano'),
+    ('GCN DK Mountain'),
+    ('Frantic Funyard'),
+    ('Wii Coconut Mall'),
+    ('GBA Bowser Castle 3'),
+    ('Kitayama Keep'),
+    ('Wii Toad''s Factory'),
+    ('3DS DK Jungle'),
+    ('N64 Bowser''s Castle'),
+    ('3DS Neo Bowser City'),
+    ('N64 DK''s Jungle Parkway'),
+    ('Tour Bowser Castle 3'),
+    ('Banished Keep'),
+    ('Thump Bump Forest'),
+    ('Wii Bowser''s Castle');
