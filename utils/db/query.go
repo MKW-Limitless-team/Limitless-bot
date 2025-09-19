886a57b2a2747786cc1a6963ef375b85c6595b0f
nwoik <nwokoyed@gmail.com>
2025-09-19 18:54:34 +0100

    added queries

---
+++ utils/db/query.go
@@ -0,0 +1,38 @@
+package db
+
+import "fmt"
+
+type SqlQuery struct {
+	text string
+}
+
+func NewQuery() *SqlQuery {
+	return &SqlQuery{}
+}
+
+func (query *SqlQuery) Select(columns ...string) *SqlQuery {
+	text := "SELECT "
+
+	if len(columns) == 0 {
+		text += "*"
+	} else {
+		for _, column := range columns {
+			if column == columns[len(columns)-1] {
+				text += column
+			} else {
+				text += fmt.Sprintf("%s,", column)
+			}
+		}
+	}
+
+	text += "\n"
+	query.text += text
+
+	return query
+}
+
+func (query *SqlQuery) From(table string) *SqlQuery {
+	query.text += "FROM " + table + "\n"
+
+	return query
+}
