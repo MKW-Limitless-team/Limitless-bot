28fbcb7e408a179c0a5190373f6b297af9ca46ad
nwoik <nwokoyed@gmail.com>
2025-09-21 13:46:26 +0100

    fixed issues

--- responses/license.go
+++ responses/license.go
@@ -22,7 +22,7 @@ func LicenseData(session *discordgo.Session, interaction *discordgo.InteractionC
 	var data *response.Data
 	userID := interaction.Member.User.ID
 
-	query := `SELECT name, friend_code, discord_id, mmr, mii 
+	query := `SELECT name, friend_code, mii, mmr 
 					FROM playerdata
 					WHERE discord_id = ?`
 	rows, err := globals.GetConnection().Query(query, userID)
@@ -32,25 +32,16 @@ func LicenseData(session *discordgo.Session, interaction *discordgo.InteractionC
 	}
 	defer rows.Close()
 
-	playerData := &ltrc.PlayerData{}
+	playerData := &ltrc.PlayerData{
+		DiscordID: userID,
+	}
 
 	// do if row.has next
-	rows.Next()
-	rows.Scan(&playerData.Name, &playerData.FriendCode, &playerData.DiscordID, &playerData.Mmr, &playerData.Mii)
-
-	query = `SELECT mii 
-					FROM playerdata
-					WHERE discord_id = ?`
-
-	mii, err := globals.GetConnection().Query(query, userID)
-
-	if err != nil {
-		return response.NewResponseData("Failed find license").InteractionResponseData
+	if rows.Next() {
+		rows.Scan(&playerData.Name, &playerData.FriendCode, &playerData.Mii, &playerData.Mmr)
+	} else {
+		return response.NewResponseData("License not found, please /register to create one.").InteractionResponseData
 	}
-	defer rows.Close()
-
-	mii.Next()
-	mii.Scan(&playerData.Mii)
 
 	data = response.NewResponseData("")
 
