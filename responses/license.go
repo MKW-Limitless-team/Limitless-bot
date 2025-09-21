56a16acba83bd4591b08442b63f985e5d31f1e8a
nwoik <nwokoyed@gmail.com>
2025-09-21 15:45:47 +0100

    optimized license code

--- responses/license.go
+++ responses/license.go
@@ -3,9 +3,9 @@ package responses
 import (
 	"fmt"
 	e "limitless-bot/components/embed"
-	"limitless-bot/globals"
 	"limitless-bot/response"
 	"limitless-bot/utils"
+	"limitless-bot/utils/db"
 	"limitless-bot/utils/ltrc"
 
 	"github.com/bwmarrin/discordgo"
@@ -22,25 +22,10 @@ func LicenseData(session *discordgo.Session, interaction *discordgo.InteractionC
 	var data *response.Data
 	userID := interaction.Member.User.ID
 
-	query := `SELECT name, friend_code, mii, mmr 
-					FROM playerdata
-					WHERE discord_id = ?`
-	rows, err := globals.GetConnection().Query(query, userID)
+	playerData, err := db.GetPlayer(userID)
 
 	if err != nil {
-		return response.NewResponseData("Failed find license").InteractionResponseData
-	}
-	defer rows.Close()
-
-	playerData := &ltrc.PlayerData{
-		DiscordID: userID,
-	}
-
-	// do if row.has next
-	if rows.Next() {
-		rows.Scan(&playerData.Name, &playerData.FriendCode, &playerData.Mii, &playerData.Mmr)
-	} else {
-		return response.NewResponseData("License not found, please /register to create one.").InteractionResponseData
+		return response.NewResponseData(err.Error()).InteractionResponseData
 	}
 
 	data = response.NewResponseData("")
--- utils/db/playerdata.go
+++ utils/db/playerdata.go
@@ -1,15 +1,18 @@
 package db
 
 import (
+	"errors"
 	"fmt"
 	"limitless-bot/globals"
+	"limitless-bot/utils/ltrc"
 )
 
-func RegisterPlayer(name string, friend_code string, discord_id string) error {
-	query := `INSERT INTO playerdata (name, friend_code, discord_id)
-					VALUES (?, ?, ?)`
+func EditMii(mii string, userID string) error {
+	query := fmt.Sprintf(`UPDATE playerdata
+				SET mii = '%s'
+				WHERE discord_id = '%s'`, mii, userID)
 
-	_, err := globals.GetConnection().Exec(query, name, friend_code, discord_id)
+	_, err := globals.GetConnection().Exec(query)
 
 	if err != nil {
 		return err
@@ -18,12 +21,33 @@ func RegisterPlayer(name string, friend_code string, discord_id string) error {
 	return nil
 }
 
-func EditMii(mii string, userID string) error {
-	query := fmt.Sprintf(`UPDATE playerdata
-				SET mii = '%s'
-				WHERE discord_id = '%s'`, mii, userID)
+func GetPlayer(userID string) (*ltrc.PlayerData, error) {
+	query := `SELECT name, friend_code, mii, discord_id, mmr 
+					FROM playerdata
+					WHERE discord_id = ?`
+	rows, err := globals.GetConnection().Query(query, userID)
 
-	_, err := globals.GetConnection().Exec(query)
+	if err != nil {
+		return nil, errors.New("failed to fetch license, ping admin")
+	}
+	defer rows.Close()
+
+	playerData := &ltrc.PlayerData{}
+
+	if rows.Next() {
+		rows.Scan(&playerData.Name, &playerData.FriendCode, &playerData.Mii, &playerData.DiscordID, &playerData.Mmr)
+	} else {
+		return nil, errors.New("license not found, please /register to create one")
+	}
+
+	return playerData, nil
+}
+
+func RegisterPlayer(name string, friend_code string, discord_id string) error {
+	query := `INSERT INTO playerdata (name, friend_code, discord_id)
+					VALUES (?, ?, ?)`
+
+	_, err := globals.GetConnection().Exec(query, name, friend_code, discord_id)
 
 	if err != nil {
 		return err
