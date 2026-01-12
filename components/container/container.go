c1fc288ea873366c65744dadf503b45e2f0472e2
nwoik <nwokoyed@gmail.com>
2026-01-12 21:17:36 +0000

    added container

---
+++ components/container/container.go
@@ -0,0 +1,27 @@
+package container
+
+import "github.com/bwmarrin/discordgo"
+
+type Container struct {
+	*discordgo.Container
+}
+
+func newContainer() *Container {
+	return &Container{&discordgo.Container{}}
+}
+
+func NewBasicContainer(color int) *Container {
+	return newContainer().SetColor(color)
+}
+
+func (container *Container) SetColor(color int) *Container {
+	container.AccentColor = &color
+
+	return container
+}
+
+func (container *Container) AddComponent(component discordgo.MessageComponent) *Container {
+	container.Components = append(container.Components, component)
+
+	return container
+}
