2bafd430047690bcd4ea8a8030375ae4f2ceb026
nwoik <nwokoyed@gmail.com>
2026-01-16 17:58:16 +0000

    added status update

--- events/ready.go
+++ events/ready.go
@@ -1,17 +1,47 @@
 package events
 
 import (
+	"encoding/json"
 	"fmt"
+	"limitless-bot/responses"
+	"net/http"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func Ready(session *discordgo.Session, event *discordgo.Ready) {
+	go getMKWIIStats(session)
+	fmt.Println("Ready to serve... ")
+}
+
+func getMKWIIStats(session *discordgo.Session) {
+	ticker := time.NewTicker(2 * time.Second)
+
+	for {
+		select {
+		case <-ticker.C:
+			resp, err := http.Get("http://localhost/api/stats?game=mariokartwii")
+
+			if err != nil {
+				UpdateStatus(session, "🎮 Mario Kart Wii: Limitless")
+			} else {
+				stats := map[string]*responses.Stats{}
+				err = json.NewDecoder(resp.Body).Decode(&stats)
+
+				global := stats["global"]
+				UpdateStatus(session, fmt.Sprintf("🎮 %d Players Online", global.OnlinePlayerCount))
+			}
+		}
+	}
+}
+
+func UpdateStatus(session *discordgo.Session, status string) {
 	session.UpdateStatusComplex(discordgo.UpdateStatusData{
 		IdleSince: nil,
 		Activities: []*discordgo.Activity{
 			{
-				Name:     "🎮 Mario Kart Wii: Limitless",
+				Name:     status,
 				Type:     discordgo.ActivityTypeGame,
 				URL:      "https://wiki.tockdom.com/wiki/Mario_Kart_Wii:_Limitless",
 				Instance: true,
@@ -20,5 +50,4 @@ func Ready(session *discordgo.Session, event *discordgo.Ready) {
 		Status: "online",
 		AFK:    false,
 	})
-	fmt.Println("Ready to serve... ")
 }
