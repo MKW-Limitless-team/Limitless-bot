3fa7fbf117a8199ab2413b10fc6e604e96679558
David Nwokoye <71194216+nwoik@users.noreply.github.com>
2026-01-22 20:12:34 +0000

    Basic display of rkg details (#22)

--- events/onMessage.go
+++ events/onMessage.go
@@ -1,10 +1,14 @@
 package events
 
 import (
+	"io"
+	"limitless-bot/messeges"
 	"limitless-bot/responses"
+	"net/http"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/nwoik/generate-mii/rkg"
 )
 
 func OnMessage(session *discordgo.Session, msg *discordgo.MessageCreate) {
@@ -13,7 +17,41 @@ func OnMessage(session *discordgo.Session, msg *discordgo.MessageCreate) {
 	if msg.Author.Bot && strings.Contains(msg.Content, "Seed:") {
 		for _, emoji := range responses.LABELS {
 			session.MessageReactionAdd(msg.ChannelID, message.ID, emoji)
-
 		}
 	}
+
+	attachments := message.Attachments
+	if !msg.Author.Bot && message.Content == "" && isTimeTrialSubmission(message.Attachments) {
+		ok := true
+		for _, attachment := range attachments {
+			resp, err := http.Get(attachment.URL)
+			if err == nil {
+				rkgData, err := io.ReadAll(resp.Body)
+				defer resp.Body.Close()
+				if err == nil {
+					readable := rkg.ConvertRkg(rkg.ParseRKG(rkgData))
+					embed := messeges.RkgEmbed(message, attachment, readable)
+					_, err := session.ChannelMessageSendEmbed(msg.ChannelID, embed)
+					if err != nil {
+						ok = false
+					}
+				}
+			}
+		}
+
+		if ok {
+			session.ChannelMessageDelete(msg.ChannelID, msg.ID)
+		}
+	}
+}
+
+func isTimeTrialSubmission(attachments []*discordgo.MessageAttachment) bool {
+	result := true
+	for _, attachment := range attachments {
+		if !strings.HasSuffix(attachment.Filename, ".rkg") {
+			result = false
+		}
+	}
+
+	return result
 }
